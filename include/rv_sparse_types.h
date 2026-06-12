9d2417f523ebe9f8b0a95a456c8e9d5d3856061c
crissmath <crissmath@users.noreply.github.com>
2026-06-12 10:00:30 -0500

    Add FP32 CSR SpGEMM correctness test

--- include/rv_sparse_types.h
+++ include/rv_sparse_types.h
@@ -4,55 +4,64 @@
 #include <stdint.h>
 
 #ifdef __cplusplus
-extern "C" {
+extern "C"
+{
 #endif
 
-typedef enum {
-    RVSP_SUCCESS = 0,
-    RVSP_ERROR_NULL_POINTER = -1,
-    RVSP_ERROR_INVALID_ARGUMENT = -2,
-    RVSP_ERROR_INVALID_CSR = -3,
-    RVSP_ERROR_UNSUPPORTED_DTYPE = -4,
-    RVSP_ERROR_UNSUPPORTED_BACKEND = -5,
-    RVSP_ERROR_ALLOCATION_FAILED = -6
-} rvsp_status_t;
+    typedef enum
+    {
+        RVSP_SUCCESS = 0,
+        RVSP_ERROR_NULL_POINTER = -1,
+        RVSP_ERROR_INVALID_ARGUMENT = -2,
+        RVSP_ERROR_INVALID_CSR = -3,
+        RVSP_ERROR_UNSUPPORTED_DTYPE = -4,
+        RVSP_ERROR_UNSUPPORTED_BACKEND = -5,
+        RVSP_ERROR_ALLOCATION_FAILED = -6
+    } rvsp_status_t;
 
-typedef enum {
-    RVSP_DTYPE_INT8,
-    RVSP_DTYPE_INT32,
-    RVSP_DTYPE_BF16,
-    RVSP_DTYPE_FP32
-} rvsp_dtype_t;
+    typedef enum
+    {
+        RVSP_DTYPE_INT8,
+        RVSP_DTYPE_INT32,
+        RVSP_DTYPE_BF16,
+        RVSP_DTYPE_FP32
+    } rvsp_dtype_t;
 
-typedef enum {
-    RVSP_FORMAT_CSR
-} rvsp_format_t;
+    typedef enum
+    {
+        RVSP_FORMAT_CSR
+    } rvsp_format_t;
 
-typedef enum {
-    RVSP_BACKEND_SCALAR,
-    RVSP_BACKEND_GCC_AUTOVEC,
-    RVSP_BACKEND_RVV_INTRINSICS
-} rvsp_backend_t;
+    typedef enum
+    {
+        RVSP_BACKEND_SCALAR,
+        RVSP_BACKEND_GCC_AUTOVEC,
+        RVSP_BACKEND_RVV_INTRINSICS
+    } rvsp_backend_t;
 
-typedef struct {
-    int32_t rows;
-    int32_t cols;
-    int32_t nnz;
+    typedef struct
+    {
+        int32_t rows;
+        int32_t cols;
+        int32_t nnz;
 
-    int32_t *row_ptr;
-    int32_t *col_idx;
-    void    *values;
+        int32_t *row_ptr;
+        int32_t *col_idx;
+        void *values;
 
-    rvsp_dtype_t dtype;
-    rvsp_format_t format;
-} rvsp_csr_matrix_t;
+        rvsp_dtype_t dtype;
+        rvsp_format_t format;
 
-typedef struct {
-    rvsp_backend_t backend;
-    rvsp_dtype_t input_dtype;
-    rvsp_dtype_t output_dtype;
-    int sort_output_indices;
-} rvsp_spgemm_options_t;
+        int owns_data;
+    } rvsp_csr_matrix_t;
+
+    typedef struct
+    {
+        rvsp_backend_t backend;
+        rvsp_dtype_t input_dtype;
+        rvsp_dtype_t output_dtype;
+        int sort_output_indices;
+    } rvsp_spgemm_options_t;
 
 #ifdef __cplusplus
 }
